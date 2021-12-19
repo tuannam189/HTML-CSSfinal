08058390da5b48bdac35ee4bf70825f154899e0b
unknown <azela1231@gmail.com>
2021-12-19 17:04:33 +0700

    first commit

---
+++ assets/js/tab.js
@@ -0,0 +1,17 @@
+function openProduct(event, productName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab--links");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(productName).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+  document.getElementById("defaultOpen").click();
+
+
+
